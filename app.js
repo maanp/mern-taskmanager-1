96e4334eadc1e6e3c9eb27a7a0c08f857dd035f2
maanp <87629198+maanp@users.noreply.github.com>
2022-03-17 19:07:28 +0530

    add port

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use("/api/v1/tasks", tasks);
 
 app.use(notFound);
 
-const port = 5000;
+const port = process.env.PORT || 5000;
 const start = async () => {
   try {
     await connectDb(process.env.MONGO_URI);
